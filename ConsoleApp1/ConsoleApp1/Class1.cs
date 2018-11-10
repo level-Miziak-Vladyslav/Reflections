902d110f89713ef63ef94f6098b8342429c05c53
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-11-10 14:03:47 +0100

    50%

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Things
+    {
+        int quantity { get; set; }
+        string name { get; set; }
+    }
+    class Car:Things
+    {
+        int capasity { get; set; }
+        void Drive()
+        {
+            Console.WriteLine("RUNNING");
+        }
+    }
+    class Tree:Things
+    {
+        int height { get; set; }
+        void Grow()
+        {
+            Console.WriteLine("GROWING");
+        }
+    }
+    class Pet:Things
+    {
+        void Feed()
+        {
+            Console.WriteLine("FEEDING");
+        }
+    }
+
+}
