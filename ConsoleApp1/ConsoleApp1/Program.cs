232ac15e3c94d6e7b2f1c3b1048627f66ad47a6c
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-11-17 14:10:12 +0100

    1711

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -25,7 +26,7 @@ namespace ConsoleApp1
 
                 Console.WriteLine("Methods supported: ");
 
-                MethodInfo[] mi = c.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                PropertyInfo[] pi = c.GetProperty(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
 
                 // Display methods supported by MyClass. 
                 foreach (MethodInfo m in mi)
@@ -47,7 +48,49 @@ namespace ConsoleApp1
 
                 Console.WriteLine();
             }
+            string str;
+            FileStream fout;
+
+            // First, open the file stream. 
+            try
+            {
+                fout = new FileStream("test.txt", FileMode.Create);
+            }
+            catch (IOException exc)
+            {
+                Console.WriteLine("Error Opening File:\n" + exc.Message);
+                return;
+            }
+
+            // Wrap the file stream in a StreamReader. 
+            StreamWriter fstr_out = new StreamWriter(fout);
+
+            try
+            {
+                Console.WriteLine("Enter text ('stop' to quit).");
+
+                do
+                {
+                    Console.Write(": ");
+                    str = Console.ReadLine();
+
+                    if (str != "stop")
+                    {
+                        str = str + "\r\n"; // add newline  
+                        fstr_out.Write(str);
+                    }
+                } while (str != "stop");
+            }
+            catch (IOException exc)
+            {
+                Console.WriteLine("I/O Error:\n" + exc.Message);
+            }
+            finally
+            {
+                fstr_out.Close();
+            }
         }
     }
-
 }
+
+
